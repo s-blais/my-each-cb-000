89c1aa7a89117248d76a976c662b5189a8bb0a7f
Stephen Blais <stephen@s-blais.com>
2019-08-21 14:58:09 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,7 @@
 def my_each (collection)
-  if block_given?
-    i=0
-    while i < collection.length
-      yield collection[i]
-      i += 1
-    end
-    #array
-  else
-    puts "No block was argued when calling the method"
+  i=0
+  while i < collection.length
+    yield collection[i]
+    i += 1
   end
 end
